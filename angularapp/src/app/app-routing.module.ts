dfe743699191d8da3718857cc731885ef6d21989
Rohinth <dev@email.com>
2022-03-13 14:32:21 +0530

    second commit

--- angularapp/src/app/app-routing.module.ts
+++ angularapp/src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SigninComponent } from './signin/signin.component';
-
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomepageComponent } from './homepage/homepage.component';
 const routes: Routes = [ 
   { path: 'users/login', component: SigninComponent},
+  {path:'About',component:AboutComponent}, 
+  {path:'Contact',component:ContactComponent},
+  {path:'homepage',component:HomepageComponent},
   { path: '', redirectTo: '/users/login', pathMatch: 'full'}
 ];
 
--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -2,9 +2,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { AboutComponent } from './about/about.component';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactComponent } from './contact/contact.component';
 import { RestServices } from './services/rest.service';
 import { SigninService } from './services/signin.service';
 import { SigninComponent } from './signin/signin.component';
@@ -13,6 +16,8 @@ import { SigninComponent } from './signin/signin.component';
   declarations: [
     AppComponent,
     SigninComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
